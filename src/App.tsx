64ab419b9e04a9c0e9aca8bc8cce372b23a7946d
MP <markpaololibunao@gmail.com>
2022-09-23 15:20:04 +0800

    - Fixes styles - Implements initial svg although it kinda failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,56 @@
-}
+import { atom, useAtom } from "jotai";
+import Layout from "./components/Layout";
+
+type Point = [number, number];
+
+const dotsAtom = atom<Point[]>([]);
+const numberOfDotsAtom = atom((get) => get(dotsAtom).length);
+
+const SvgDots = () => {
+  const [dots] = useAtom(dotsAtom);
+  return (
+    <g width="100%" height="100%" viewBox="0 0 100% 100%">
+      {dots.map(([x, y], i) => (
+        <circle cx={x} cy={y} r="2" fill="#aaa" />
+      ))}
+    </g>
+  );
+};
+
+export const SvgRoot = () => {
+  const [, setDots] = useAtom(dotsAtom);
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 100% 100%"
+      onMouseMove={(e) => {
+        const p: Point = [e.clientX, e.clientY];
+        setDots((prev) => [...prev, p]);
+      }}
+    >
+      <rect width="100%" height="100%" fill="#eee" />
+      <SvgDots />
+    </svg>
+  );
+};
+
+export const Stats = () => {
+  const [numberOfDots] = useAtom(numberOfDotsAtom);
+  return (
+    <ul>
+      <li>Dots: {numberOfDots}</li>
+    </ul>
+  );
+};
+
 function App() {
-export default App
+  return (
+    <Layout>
+      <SvgRoot />
+      <Stats />
+    </Layout>
+  );
+}
+
+export default App;
