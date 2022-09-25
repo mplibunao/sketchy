e29e389a606b4c203e83cbe10e98f508cdaf625b
MP <markpaololibunao@gmail.com>
2022-09-25 17:05:49 +0800

    Automatically selects the last created shape

--- src/components/SvgShapes.tsx
+++ src/components/SvgShapes.tsx
@@ -1,5 +1,6 @@
 import { atom, useAtom } from 'jotai'
 import { Point, ShapeAtom } from '../types'
+import { selectAtom } from './selection'
 import { createShapeAtom, SvgShape } from './SvgShape'
 
 const shapeAtomsAtom = atom<ShapeAtom[]>([])
@@ -7,6 +8,7 @@ const shapeAtomsAtom = atom<ShapeAtom[]>([])
 export const addShapeAtom = atom(null, (_get, set, point: readonly Point[]) => {
 	const shapeAtom = createShapeAtom(point)
 	set(shapeAtomsAtom, (prev) => [...prev, shapeAtom])
+	set(selectAtom, shapeAtom)
 })
 
 export const SvgShapes = () => {
